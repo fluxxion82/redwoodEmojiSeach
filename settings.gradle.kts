f1319c1d5a5eb355c398eec3977ff49535283e2a
Sterling Albury <fluxxion@gmail.com>
2023-01-31 23:43:57 -0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "emojiSearch"
+
+include(":values")
+include(":schema")
+include(":schema:compose")
+include(":schema:compose:protocol")
+include(":schema:widget")
+include(":schema:widget:protocol")
+include(":presenter")
+include(":presenter-treehouse")
+include(":launcher")
+
+include(":android-composeui")
